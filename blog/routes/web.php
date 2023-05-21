d1da2c6bbc037ab519234ec3b4f10dae38f20c82
enjelhpg <enjelhpg@gmail.com>
2023-05-21 20:33:42 +0800

    next

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,17 +16,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "enjel",
         "email" => "enjelhpg@gmail.com",
         "image" => "img/enjel.jpg"
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('posts');
-});
+
+Route::get('/blog', [PostController::class, 'index'] );
+
+//halaman single post
+Route::get('posts/{post:slug}',[PostController::class, 'show']);
