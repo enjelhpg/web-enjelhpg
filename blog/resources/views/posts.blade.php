36f9b30d79faf452426ea5148d33074485325584
enjelhpg <enjelhpg@gmail.com>
2023-05-22 09:41:22 +0800

    memperbaiki

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -2,9 +2,10 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-5">Halaman Blog Post</h1>
 
 @foreach ($posts as $post)
-<article class="mb-5">
+<article>
 <h2><a href="/posts/{{  $post->slug }}">{{ $post->title }}</a></h2>
 <p>{{ $post->excerpt }}</p>
 </article>
